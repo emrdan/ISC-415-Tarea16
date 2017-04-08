737c16d682831bff6a251583706f1508d001a10f
Daniel Mendez <emrdan@Daniels-MacBook-Pro.local>
2017-04-08 00:49:40 -0400

    finally..

--- app/movier/__init__.py
+++ app/movier/__init__.py
@@ -1,4 +1,4 @@
-from flask import abort, Flask, g, render_template, request
+from flask import abort, Flask, g, render_template, request, send_from_directory
 from flask_cors import CORS
 from movier.main.routes import main
 from movier.config import configure_app
@@ -17,6 +17,17 @@ app.register_blueprint(main, url_prefix='/')
 
 @app.route("/")
 def home():
-    print(app.config)
-    return app.send_static_file('reviews.html')
+  return app.send_static_file("reviews.html");
+
+@app.route('/<path:filename>')
+def serve_html(filename):
+    return send_from_directory("static", filename, as_attachment=False)
+
+@app.route('/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory("static/css", filename, as_attachment=False)
+
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory("static/js", filename, as_attachment=False)
 
