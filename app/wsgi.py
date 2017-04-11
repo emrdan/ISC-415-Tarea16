5bda3629d86284bf6ef2a84fc738f694e994f8d6
Daniel <emrdan@gmail.com>
2017-04-11 18:55:12 -0400

    almost..

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,5 +1,5 @@
 from movier import application
 
 if __name__ == '__main__':
-    application.run(host = '0.0.0.0', port = 7200)
+    application.run(host = '0.0.0.0', port = 9520)
     application.debug = True
